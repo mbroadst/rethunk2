b058207c353785abc33637a5e87db5cca39d2693
Matt Broadstone <mbroadst@gmail.com>
2017-01-06 08:48:28 -0500

    test(runner): improve basic bash test runner for polyglot

--- test/polyglot/run.sh
+++ test/polyglot/run.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
 SCRIPTS=`find . -type f \( -name "*.js" -and ! -name "common.js" \)`
-for SCRIPT in $SCRIPTS; do $SCRIPT; done
 
+FAILED=0
+FAILED_RX="Failed ([0-9]+) tests"
+for SCRIPT in $SCRIPTS; do
+  OUTPUT=$( $SCRIPT )
+  if [[ $OUTPUT =~ ^Passed ]]; then
+    echo "$(basename $SCRIPT): passed"
+  else
+    if [[ $OUTPUT =~ $FAILED_RX ]]; then
+      FAILED=$((FAILED + BASH_REMATCH[1]))
+    fi
+
+    echo "$(basename $SCRIPT):"
+    echo "${OUTPUT}"
+  fi
+done
+
+echo "$FAILED tests failed"
